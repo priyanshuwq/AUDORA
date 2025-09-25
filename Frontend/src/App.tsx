345352ddb7362d05bdd1e441b6ebe89c5c055a6d
priyanshushekhar07 <priyanshuofficial2004@gmail.com>
2025-09-25 18:16:46 +0530

    vite and Shadcn UI

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+return (
+    <>
+      <h1 className="text-3xl font-bold text-red-500">Hello World</h1>
+    <Button>Click Me </Button>
+    </>
+  );
+}
+
+export default App
